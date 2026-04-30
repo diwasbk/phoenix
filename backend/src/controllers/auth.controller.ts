b18dd62439b513c52a984879d8a5d52df6e81bb2
diwasbk <diwasbk903@gmail.com>
2026-05-01 04:20:51 +0545

    feat(auth): implement login authentication with JWT cookie handling

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { userModel } from "../models/user.model";
 import bcrypt from "bcrypt";
+import { generateToken } from "../utils/jwt";
 
 class AuthController {
     // Signup User
@@ -39,6 +40,59 @@ class AuthController {
             });
         };
     };
+
+    // Login User
+    loginUser = async (req: Request, res: Response) => {
+        try {
+            const { email, password } = req.body;
+
+            const userExist = await userModel.findOne({ email: email });
+
+            if (!userExist) {
+                return res.status(404).send({
+                    message: "Invalid email or password!",
+                    success: false
+                });
+            };
+
+            const isPasswordMatch = await bcrypt.compare(password, userExist.password);
+
+            if (!isPasswordMatch) {
+                return res.status(401).send({
+                    message: "Invalid email or password!",
+                    success: false
+                });
+            };
+
+            const payload = {
+                id: userExist._id.toString(),
+                email: userExist.email,
+                role: userExist.role
+            };
+
+            const auth_token = generateToken(payload);
+
+            res.cookie("auth_token", auth_token, {
+                httpOnly: true,
+                maxAge: 3000 * 1000,
+                sameSite: "lax",
+                secure: false
+            });
+
+
+            res.status(200).send({
+                message: "Logged in successfully!",
+                success: true
+            });
+
+        } catch (err: any) {
+            console.log(err);
+            res.status(500).send({
+                message: err.message ? `Internal server error: ${err.message}` : "Internal server error.",
+                success: true
+            });
+        }
+    }
 };
 
 export default AuthController;
--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import AuthController from "../controllers/auth.controller";
 import schemaValidateMiddleware from "../middlewares/schema.validate.middleware";
-import { signupSchema } from "../types/auth.types";
+import { loginSchema, signupSchema } from "../types/auth.types";
 
 const authRouter = express.Router();
 const authController = new AuthController();
 
 authRouter.post("/signup", schemaValidateMiddleware(signupSchema), authController.signupUser);
+authRouter.post("/login", schemaValidateMiddleware(loginSchema), authController.loginUser);
 
 export default authRouter;
