4fd90864e4b37eef08e42229cd05944b1f9cba30
diwasbk <diwasbk903@gmail.com>
2026-05-02 14:15:29 +0545

    feat(application): add application API and server actions

---
+++ frontend/app/lib/actions/application-actions.ts
@@ -0,0 +1,128 @@
+import { applicationType } from "@/app/(public)/apply/schema";
+import { deleteApplicationByID, getAllApplication, getApplicationByID, submitApplication, updateApplicationDetailByID } from "../api/application";
+
+export const handleSubmitApplication = async (data: applicationType) => {
+    try {
+        const result = await submitApplication(data);
+
+        if (!result.success) {
+            return {
+                message: result.message || "Failed to submit application!",
+                success: false
+            };
+        };
+
+        return {
+            message: result.message || "Application submitted successfully!",
+            success: true
+        };
+
+    } catch (err: Error | any) {
+        return {
+            message: err.message || "Failed to submit application!",
+            success: false
+        };
+    };
+};
+
+// Handle Get All Application
+export const handleGetAllApplication = async () => {
+    try {
+        const result = await getAllApplication();
+
+        if (!result.success) {
+            return {
+                message: result.message || "Failed to fetch applications",
+                success: false
+            };
+        };
+
+        return {
+            message: result.message || "Applications fetched successfully!",
+            result: result.result,
+            success: true
+        };
+    } catch
+    (err: Error | any) {
+        return {
+            message: err.message || "Failed to fetch applications!",
+            success: false
+        };
+    };
+};
+
+// Handle Get Application By ID
+export const handleGetApplicationByID = async (applicationId: string) => {
+    try {
+        const result = await getApplicationByID(applicationId);
+
+        if (!result.success) {
+            return {
+                message: result.message || "Failed to fetch application",
+                success: false
+            };
+        };
+
+        return {
+            message: result.message || "Application fetched successfully!",
+            result: result.result,
+            success: true
+        };
+
+    } catch (err: Error | any) {
+        return {
+            message: err.message || "Failed to fetch application!",
+            success: false
+        };
+    };
+};
+
+// Handle Update Application Detail
+export const handleUpdateApplicationDetailByID = async (applicationId: string, data: applicationType) => {
+    try {
+        const result = await updateApplicationDetailByID(applicationId, data);
+
+        if (!result.success) {
+            return {
+                message: result.message || "Failed to update application!",
+                success: false
+            };
+        };
+
+        return {
+            message: result.message || "Application updated successfully!",
+            success: true
+        };
+
+    } catch (err: Error | any) {
+        return {
+            message: err.message || "Failed to update application!",
+            success: false
+        };
+    };
+};
+
+// Handle Delete Application By ID
+export const handleDeleteApplicationByID = async (applicationId: string) => {
+    try {
+        const result = await deleteApplicationByID(applicationId);
+
+        if (!result.success) {
+            return {
+                message: result.message || "Failed to delete application",
+                success: false
+            };
+        };
+
+        return {
+            message: result.message || "Application deleted successfully!",
+            success: true
+        };
+
+    } catch (err: Error | any) {
+        return {
+            message: err.message || "Failed to delete application!",
+            success: false
+        };
+    };
+};
---
+++ frontend/app/lib/api/application.ts
@@ -0,0 +1,63 @@
+import { applicationType } from "@/app/(public)/apply/schema";
+import axiosInstance from "./axios";
+import API from "./endpoints";
+
+// Submit Application
+export const submitApplication = async (data: applicationType) => {
+    try {
+        const response = await axiosInstance.post(API.APPLICATION.SUBMIT, data);
+
+        return response.data;
+
+    } catch (err: Error | any) {
+        throw new Error(err.response?.data?.message || err.response || "Failed to submit application!");
+    };
+};
+
+// Get All Application
+export const getAllApplication = async () => {
+    try {
+        const response = await axiosInstance.get(API.APPLICATION.GET_ALL);
+
+        return response.data;
+
+    } catch (err: Error | any) {
+        throw new Error(err.response?.data?.message || err.response || "Failed to fetch applications!");
+    };
+};
+
+// Get Application By ID
+export const getApplicationByID = async (applicationId: string) => {
+    try {
+        const response = await axiosInstance.get(API.APPLICATION.GET_BY_ID(applicationId));
+
+        return response.data;
+
+    } catch (err: Error | any) {
+        throw new Error(err.response?.data?.message || err.response || "Failed to fetch application!");
+    };
+};
+
+// Update Application Detail By ID
+export const updateApplicationDetailByID = async (applicationId: string, data: applicationType) => {
+    try {
+        const response = await axiosInstance.put(API.APPLICATION.UPDATE_BY_ID(applicationId), data);
+
+        return response.data;
+
+    } catch (err: Error | any) {
+        throw new Error(err.response?.data?.message || err.response || "Failed to update application detail!");
+    };
+};
+
+// Delete Application By ID
+export const deleteApplicationByID = async (applicationId: string) => {
+    try {
+        const response = await axiosInstance.delete(API.APPLICATION.DELETE_BY_ID(applicationId));
+
+        return response.data;
+
+    } catch (err: Error | any) {
+        throw new Error(err.response?.data?.message || err.response || "Failed to delete application!");
+    };
+};
