52b779a280f45046e7ed5e699bd0468e88497915
diwasbk <diwasbk903@gmail.com>
2026-07-09 00:47:03 +0545

    feat(middlewares): add owner or admin authorization middleware

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -3,6 +3,7 @@ import AuthController from "../controllers/auth.controller";
 import schemaValidateMiddleware from "../middlewares/schema.validate.middleware";
 import { changePasswordSchema, loginSchema, requestPasswordResetEmailSchema, resetPasswordSchema, signupSchema } from "../types/auth.types";
 import { jwtAuthMiddleware } from "../utils/jwt";
+import { isOwnerOrAdminAuthMiddleware } from "../middlewares/authorize.middleware";
 
 const authRouter = express.Router();
 const authController = new AuthController();
@@ -12,6 +13,6 @@ authRouter.post("/login", schemaValidateMiddleware(loginSchema), authController.
 authRouter.patch("/change-password", jwtAuthMiddleware, schemaValidateMiddleware(changePasswordSchema), authController.changePassword);
 authRouter.post("/request-password-reset-email", schemaValidateMiddleware(requestPasswordResetEmailSchema), authController.requestPasswordResetEmail);
 authRouter.patch("/reset-account-password", schemaValidateMiddleware(resetPasswordSchema), authController.resetAccountPassword);
-authRouter.delete("/delete-account/:userId", jwtAuthMiddleware, schemaValidateMiddleware(loginSchema.pick({ password: true })), authController.deleteUserAccountByUserId)
+authRouter.delete("/delete-account/:userId", jwtAuthMiddleware, isOwnerOrAdminAuthMiddleware, schemaValidateMiddleware(loginSchema.pick({ password: true })), authController.deleteUserAccountByUserId)
 
 export default authRouter;
