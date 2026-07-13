d418ebd5fdbbcd68d7b330d304573cfd7c24abff
diwasbk <diwasbk903@gmail.com>
2026-07-14 00:15:03 +0545

    feat(auth): add 2FA login verification page

---
+++ frontend/app/(auth)/login/2fa/page.tsx
@@ -0,0 +1,11 @@
+import NavBar from "@/app/(public)/_components/navbar";
+import TwoFactorLoginPage from "../_components/two-factor-login-page";
+
+export default function Page() {
+    return (
+        <div>
+            <NavBar/>
+            <TwoFactorLoginPage/>
+        </div>
+    );
+}
